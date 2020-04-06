837ab97873764da5967316ba8869df42abb22578
塩谷　佳規 <>
2020-04-06 23:22:09 +0900

    4/6 23:22

--- app/src/main/java/com/example/sensorget/MainActivity.kt
+++ app/src/main/java/com/example/sensorget/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.sensorget
 
 import android.content.Context
+import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT_WATCH)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,8 +24,25 @@ class MainActivity : AppCompatActivity() {
         val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         val sList : List<Sensor> = sensorManager.getSensorList(Sensor.TYPE_ALL)
         for (sensor : Sensor in sList ) {
-            buffer.append("${sensor.type},${sensor.name},${sensor.vendor}")
+            buffer.append("${sensor.stringType},${sensor.name},${sensor.vendor}\n")
         }
         txt01.text = buffer.toString()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.accelSensor_menu -> {
+             val intent = Intent(this,AccelGet::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
